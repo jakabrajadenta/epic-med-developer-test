ee41dce5f8d992be8ab7b345faf28868b2afb6ff
Jaka Brajadenta <79687223+jakabrajadenta@users.noreply.github.com>
2026-01-06 16:52:32 +0700

    Init project

---
+++ src/test/java/com/epicmed/developer/assesment/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicmed.developer.assesment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
